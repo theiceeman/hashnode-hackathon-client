d746de1c0e9ccf66850ee353b5a59d8946731ef8
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-01-12 01:18:37 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { ThirdwebProvider, ConnectWallet } from '@3rdweb/react';
 import './App.css';
+import TokenComponent from './components/token';
+const supportedChainIds = [1, 4, 137, 250, 43114, 80001];
+const connectors = {
+  injected: {},
+  magic: { 
+  },
+  walletconnect: {},
+  walletlink: {
+    appName: "thirdweb - demo",
+    url: "https://thirdweb.com",
+    darkMode: false,
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ThirdwebProvider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <ConnectWallet />
+      <TokenComponent />
+    </ThirdwebProvider>
     </div>
-  );
+  )
 }
 
 export default App;
