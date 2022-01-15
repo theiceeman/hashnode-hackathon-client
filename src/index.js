dfdbd95fd0d035eff61e1e8ecf5beda456cef76d
izaakwalz <iblack.xyz@gmail.com>
2022-01-15 18:50:45 +0100

    📦 Added new packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import './index.css'; // main css
+// flowbite js
+import '@themesberg/flowbite';
+// tailwind css styles
 import './css/tailwind.css';
 
 import App from './App';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@themesberg/flowbite/**/*.js'],
 	theme: {
 		extend: {
 			fontFamily: {
@@ -17,7 +17,28 @@ module.exports = {
 					'"Segoe UI Symbol"',
 				],
 			},
+			colors: {
+				grey: {
+					50: '#F9FAFB',
+					100: '#F4F6F8',
+					200: '#E6E5E8',
+					300: '#C4CDD5',
+					400: '#e6e5e8',
+					500: '#adb0bb',
+					600: '#949db2',
+					700: '#767E89',
+					800: '#32363E',
+					900: '#20232A',
+					dark: '#212121',
+					darker: '#20232A',
+				},
+				secondary: {
+					lighter: '#FFD3BC',
+					light: '#FFC5A6',
+					main: '#FB9678',
+				},
+			},
 		},
 	},
-	plugins: [],
+	plugins: [require('@themesberg/flowbite/plugin')],
 };
