6c815ea6910d92584e6d33d31a11638d916c9f1b
izaakwalz <iblack.xyz@gmail.com>
2022-01-20 04:16:50 +0100

    feat: added `darrk-mode` hooks

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Logo = ({ className }) => {
-	return <img className={className ?? ' w-[200px] h-10'} src='/images/logo-light.svg' alt='logo' />;
+	const theme = localStorage.getItem('color-theme');
+	return (
+		<img
+			className={className ?? 'w-full'}
+			src={theme === 'dark' ? '/images/logo-dark.svg' : '/images/logo-light.svg'}
+			alt='logo'
+		/>
+	);
 };
 
 export default Logo;
