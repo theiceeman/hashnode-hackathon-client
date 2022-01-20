d4a59326cd331c3e44f6b6ad85be06b3e2462b5c
izaakwalz <iblack.xyz@gmail.com>
2022-01-20 04:19:13 +0100

    feat: added errorPage & changed route to dashboard

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import { useRoutes } from 'react-router-dom';
 import MainLayout from 'layouts/MainLayout';
 import DashboardLayout from 'layouts/dashboard';
 // pages
-import { HomePage, WalletPage } from 'pages';
+import { Error404Page, HomePage, WalletPage } from 'pages';
 
 export default function Routes() {
 	return useRoutes([
 		{
-			path: '/app',
+			path: '/dashboard',
 			element: <DashboardLayout />,
 			children: [{ path: '', element: <WalletPage /> }],
 		},
@@ -19,5 +19,6 @@ export default function Routes() {
 			element: <MainLayout />,
 			children: [{ path: '/', element: <HomePage /> }],
 		},
+		{ path: '*', element: <Error404Page /> },
 	]);
 }
