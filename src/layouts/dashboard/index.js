a010d5976774274be274f11681c492317a9099a5
kelvin <okorieebube1@gmail.com>
2022-02-08 08:50:27 +0100

    feat: storing `interface theme` in redux state

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,42 +1,60 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
 // Dashboard components
-import Footer from './Footer';
-import Navbar from './Navbar';
-import TopBar from './TopBar';
-import { HiExclamation } from 'react-icons/hi';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import TopBar from "./TopBar";
+import { HiExclamation } from "react-icons/hi";
+import { useContractCall } from "@usedapp/core";
+import { Contract } from "ethers";
 
 const Notification = () => {
-	return (
-		<div className='w-full text-white bg-norm-blue'>
-			<div className='container flex items-center px-3 py-1 mx-auto'>
-				<HiExclamation className='w-5 h-5 text-white' />
-				<p className='mx-3 font-semibold font-sans text-sm'>
-					Please make sure you have Metamask installed on your PC.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full text-white bg-norm-blue">
+      <div className="container flex items-center px-3 py-1 mx-auto">
+        <HiExclamation className="w-5 h-5 text-white" />
+        <p className="mx-3 font-semibold font-sans text-sm">
+          Please make sure you have Metamask installed on your PC.
+        </p>
+      </div>
+    </div>
+  );
 };
 
-const DashboardLayout = ({ account}) => {
-	return (
-		<>
-			<div className='bg-nature-100 dark:bg-nature-900'>
-				<div className='w-full'>
-					{/* Navigation starts */}
-					{window && !window.ethereum && <Notification />}
-					<Navbar account={account}/>
-					<TopBar />
-					{/* Navigation ends */}
-					<main className='lg:container lg:mx-auto py-12 w-full lg:px-8'>
-						<Outlet />
-					</main>
-				</div>
-				<Footer />
-			</div>
-		</>
-	);
+const DashboardLayout = ({ account }) => {
+  function useTokenBalance(tokenAddress, address) {
+    const { value, error } =
+      useContractCall(
+        address &&
+          tokenAddress && {
+            contract: new Contract(tokenAddress, ERC20Interface), // instance of called contract
+            method: "balanceOf", // Method to be called
+            args: [address], // Method arguments - address to be checked for balance
+          }
+      ) ?? {};
+    if (error) {
+      console.error(error.message);
+      return undefined;
+    }
+    return value?.[0];
+  }
+  return (
+    <>
+      <div className="bg-nature-100 dark:bg-nature-900">
+        <div className="w-full">
+          {/* Navigation starts */}
+          {window && !window.ethereum && <Notification />}
+          <Navbar account={account} />
+          <TopBar />
+          {/* Navigation ends */}
+          <main className="lg:container lg:mx-auto py-12 w-full lg:px-8">
+            <Outlet />
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default DashboardLayout;
